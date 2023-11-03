b90ef22cef7a22951390f0e7c16eea6a15680cba
Sheng Chen <sheng.chen@hexagon.com>
2023-11-03 16:42:16 +0800

    build for cmake

---
+++ Program1/helper/helper.cpp
@@ -0,0 +1,18 @@
+#include "helper.h"
+#include <iostream>
+
+using namespace std;
+void helper::start()
+{
+    cout << "start ..." << endl;
+}
+
+void helper::progress()
+{
+    cout << "progress ..." << endl;
+}
+
+void helper::end()
+{
+    cout << "end ..." << endl;
+}
--- Program1/main.cpp
+++ Program1/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <../helper/helper.h>
 
 int main(int argc, char* argv[])
 {
+    helper help;
+    help.start();
+    help.progress();
+    help.end();
     std::cout << "Welcome to C++ Program" << std::endl;
     return 0;
 }
