74653f25405322f56bae9ec297b29538aac3093b
Sheng Chen <sheng.chen@hexagon.com>
2023-11-22 15:56:10 +0800

    new

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <helper.h>
 
+class A
+{
+    public:
+    void upload() {
+        std::cout << "A upload" << std::endl;
+    }
+};
+
+class B
+{
+    public:
+    void upload() {
+        std::cout << "B upload" << std::endl;
+    }
+};
+
+template <typename T>
+void uploadT(T& t)
+{
+    t.upload();
+}
+
 int main(int argc, char* argv[])
 {
     helper help;
     help.start();
     help.progress();
     help.end();
+    A a;
+    B b;
+    uploadT(a);
+    uploadT(b);
     std::cout << "Welcome to C++ Program" << std::endl;
     return 0;
 }
